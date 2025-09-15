52763d6215804a52ed3481f8fd3edc9936c2b457
Luis Alejandro Montenegro Nájera <luigiale14@gmail.com>
2025-09-15 15:10:29 -0600

    Inicio desde 0

--- Ejercicio_universidad.py/Classcurso.py
+++ Ejercicio_universidad.py/Classcurso.py
@@ -1,8 +1,10 @@
 from Classfacultad import facultad
 
 class curso(facultad):
-    def __init__(self, titulo_curso, code, cursos, profesor):
-        super().__init__(cursos)
+    def __init__(self, titulo_curso, code, cursos, profesor, nombre_facultad):
+        super().__init__(cursos, nombre_facultad)
         self.titulo_curso = titulo_curso
         self.code = code
         self.profesor = profesor
+
+        
--- Ejercicio_universidad.py/Classuniversidad.py
+++ Ejercicio_universidad.py/Classuniversidad.py
@@ -1,12 +1,15 @@
 class universidad:
     def __init__(self,facultad):
         self.facultad = facultad if facultad is not None else []
+        self.profesor = []
         
     def agregar_facultades(self,faculty):
         self.facultad.append(faculty)
         print(f'Se ha agregado {faculty} correctamente!!')
         print(self.facultad)
     
+    def ver_facultades(self):
+        print(self.facultad)
 
     def agregar_profesor(self,professor):
         pass
@@ -15,6 +18,8 @@ class universidad:
 
 UFM = universidad(['Economía'])
 UFM.agregar_facultades('Ingenieria')
+UFM.ver_facultades()
+UFM.agregar_profesor('Julio')
         
 
 
